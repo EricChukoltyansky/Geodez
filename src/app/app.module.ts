27debd8eed9bbca78961696e128ea136947b59d8
EricChukoltyansky <kazohelmet@gmail.com>
2022-07-26 20:28:27 +0300

    navbar design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, NavbarComponent],
+  declarations: [AppComponent, MainComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
