a22b4daef94cc7667024aec3cfe6dcda350e2e74
EricChukoltyansky <kazohelmet@gmail.com>
2022-08-02 13:37:27 +0300

    moving div upwards on button press

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  toggleInfoContainer: boolean = false;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleShowInformation() {
+    this.toggleInfoContainer = true;
   }
-
 }
