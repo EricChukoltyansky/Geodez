a9fe4ee8e03d093ac1b4b1b1cde6459d292103a8
EricChukoltyansky <kazohelmet@gmail.com>
2022-08-02 18:37:15 +0300

    add angular animation to the information container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -9,7 +10,7 @@ import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [AppComponent, MainComponent, NavComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,9 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
+  animations: [
+    trigger('transitionTrigger', [
+      state(
+        'true',
+        style({
+          alignItems: 'center',
+          flexDirection: 'column',
+          color: '#fff',
+          display: 'flex',
+          height: '30%',
+        })
+      ),
+      transition('false => true', [animate('1s ease-in')]),
+    ]),
+  ],
 })
 export class MainComponent implements OnInit {
   constructor() {}
